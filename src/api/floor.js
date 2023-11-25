01a1e98f03f3e9557ae78b824dc24cd43f5a1944
runnywolf <flowynnur@gmail.com>
2023-11-25 18:42:59 +0800

    Edit comment in api file.

--- src/api/floor.js
+++ src/api/floor.js
@@ -1,33 +1,64 @@
-import classroomDB from "@/api/classroom-data.json";
+// !!! 警告 !!!
+// 嘗試修改不是"可修改區"的code,會導致不可預期的錯誤!
+
+import classroomDB from "@/api/classroom-data.json"; // 後端實裝後可刪
 
 export function getClassroomData(classroomID){ // 跟後端請求某個教室的資料
   const classroom_default = {
-    info: { building: "未知的大樓", name: "未知", number: "-", equipment: "-" },
-    isSave: false,
-    periodData: []
-  } // 預設教室資料,勿刪
+    id: null, // 教室的唯一id, example: "ins101"
+    info: { building: "未知的大樓", name: "未知", number: "-", equipment: "-" }, // 教室的基本資料 (不變)
+    isSave: false, // 教室是否被user收藏
+    periodData: [] // 教室被所有人借的情況
+  };
   
   let classroom = classroom_default; // 如果找不到資料則回傳default
+  
+  // 可修改區 start
   if (classroomID in classroomDB.C) classroom = classroomDB.C[classroomID];
+  // 可修改區 end
+  
   classroom.id = classroomID;
   
   return classroom;
 }
-
-export function updateSave(classroomID, isSave){ // 修改user的收藏data
-  alert(`[api/floor/updateSave]\nclassroomID = ${classroomID}\nisSave = ${isSave}`); // debug
-  return 1;
-}
-/* return:
-  1: update success
-  -1: update failed
+/*
+  input:
+    classroomID: <string>教室的唯一id, example: "ins101"
+  
+  return:
+    classroom: 請參考 function getClassroomData() 內的 classroom_default
 */
 
-export function sendApply(selectedPeriod){ // 送出借用教室的申請
+export function updateSave(classroomID, isSave){ // 修改user的收藏data
+  // 可修改區 start
+  alert(`[api/floor/updateSave]\nclassroomID = ${classroomID}\nisSave = ${isSave}`); // debug
+  return 1;
+  // 可修改區 end
+}
+/*
+  input:
+    classroomID: <string>教室的唯一id, example: "ins101"
+    isSave: <bool>是否收藏 -> 更新後端
+  userName: <string>
+
+  return:
+    1: update success
+    -1: update failed
+*/
+
+export function sendApply(classroom, selectedPeriod){ // 送出借用教室的申請
+  // 可修改區 start
   alert(`[api/floor/sendApply]\nselectedPeriod = ${selectedPeriod}`); // debug
   return 1;
+  // 可修改區 end
 }
-/* return:
-  1: apply success
-  -1: apply failed
+/*
+  input:
+    classroom: 同 function getClassroomData() 裡的 classroom
+    selectedPeriod: { day: <int>星期幾, startPeriod: <int>開始節數, endPeriod: <int>結束節數 }
+  userName: <string>
+  
+  return:
+    1: apply success
+    -1: apply failed
 */
--- src/api/login.js
+++ src/api/login.js
@@ -1,38 +1,65 @@
-export function getPasswordHint(account){
+// !!! 警告 !!!
+// 嘗試修改不是"可修改區"的code,會導致不可預期的錯誤!
+
+export function getPasswordHint(account){ // 獲得某個帳號的密碼提示
+  let hint = null; // 預設回傳的密碼提示
+  
+  // 可修改區 start
   alert(`[api/login/getPasswordHint]\naccount = ${account}`); // debug
-  const hint = "你知道的"
-  if (account == "1") return hint;
-  return null;
+  if (account == "1") hint = "你知道的"; // 如果帳號存在,將 hint 設為該帳號的密碼提示
+  // 可修改區 end
+  
+  return hint;
 }
-/* return:
-  帳號存在,回傳 hint
-  帳號不存在, return null
+/*
+  input:
+    account: <string>輸入框的帳號
+  
+  return:
+    hint: <string>密碼提示 | <null>
 */
 
-export function userLogin(account, password){
+export function userLogin(account, password){ // 送出登入請求
+  // 可修改區 start
   alert(`[api/login/userLogin]\naccount = ${account}\npassword = ${password}`); // debug
   if (account == "1" && password == "1") return 1;
   return -1;
+  // 可修改區 end
 }
-/* return:
-  1: 登入成功
-  -1: 帳號或密碼錯誤
-  -2: 帳號遭到封鎖, 解封時間: <time>
+/*
+  input:
+    account: <string>輸入框的帳號
+    password: <string>輸入框的密碼
+  
+  return:
+    if 登入成功 -> return 1
+    if 帳號或密碼錯誤 -> return -1
+    if 帳號遭到封鎖 -> return -2
 */
 
-export function userRegister(account, password, hint){
+export function userRegister(account, password, hint){ // 送出註冊帳號請求
+  // 可修改區 start
   alert(`[api/login/userRegister]\naccount = ${account}\npassword = ${password}\nhint = ${hint}`); // debug
   if (account == "" || password == "" || hint == "") return -2;
   if (account == "1" && password == "1") return 2;
   return 1;
+  // 可修改區 end
 }
-/* return:
-  2: 帳號已註冊,密碼正確,將自動登入
-  1: 註冊成功,將自動登入
-  -1: 帳號已存在
-  -2: 註冊資料未輸入完整
+/*
+  input:
+    account: <string>輸入框的帳號
+    password: <string>輸入框的密碼
+    hint: <string>密碼提示
+  
+  return:
+    if 註冊成功 -> return 1
+    if 帳號已存在,密碼正確 -> return 2
+    if 帳號已存在,密碼錯誤 -> return -1
+    if 註冊資料未輸入完整 -> return -2
 */
 
-export function userLogout(){
+export function userLogout(){ // 登出
+  // 可修改區 start
   alert("[api/login/userLogout]"); // debug
+  // 可修改區 end
 }
