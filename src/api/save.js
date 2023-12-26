6272f86fd06425ce530fd6f3c5323a76dd69b89e
elisa40334 <148987483+elisa40334@users.noreply.github.com>
2023-12-27 00:03:13 +0800

    收藏功能

--- src/api/save.js
+++ src/api/save.js
@@ -1,13 +1,13 @@
 // import user from "@/api/user-data.json"; // 後端完成後可刪
 
-export function getIsSave() { // user是否有收藏某間教室
+export function getIsSave(classroomID) { // user是否有收藏某間教室
     var arr = JSON.parse(localStorage.getItem('issave'));
     if (arr == null) {
         arr = [];
     }
     console.log(arr)
-        // return arr.find(classroomID);
-    return false;
+    return arr.includes(classroomID);
+    // return false;
 }
 /*
 input:
@@ -34,10 +34,16 @@ userAccount: <string>
 */
 
 export function removeSave(classroomID) { // 移除收藏
-    var arr = localStorage.getItem('issave');
-    arr.remove(classroomID);
-    localStorage.issave = arr;
-    // alert(`[api/save/removeSave]\nclassroomID = ${classroomID}`); // debug
+    var arr = JSON.parse(localStorage.getItem('issave'));
+    if (arr == null) {
+        arr = [];
+    }
+    const index = arr.indexOf(classroomID);
+    if (index > -1) { // only splice array when item is found
+        arr.splice(index, 1); // 2nd parameter means remove one item only
+    }
+    localStorage.setItem('issave', JSON.stringify(arr))
+        // alert(`[api/save/removeSave]\nclassroomID = ${classroomID}`); // debug
 }
 /*
 input:
@@ -46,7 +52,7 @@ userAccount: <string>
 */
 
 export function getAllSave() { // user的全部收藏
-    var arr = localStorage.getItem('issave');
+    var arr = JSON.parse(localStorage.getItem('issave'));
     if (arr == null) {
         arr = [];
     }
