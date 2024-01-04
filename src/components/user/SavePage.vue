d013b4b46455247b39ab97876c9f0e953f76fe7a
yuan <scott680313@gmail.com>
2024-01-04 12:01:06 +0800

    savepage的小改動，可以iterable

--- src/components/user/SavePage.vue
+++ src/components/user/SavePage.vue
@@ -1,21 +1,42 @@
 <template>
-  <data-table :insert-data="getInsertData()"/>
+  <data-table :insert-data="insertData" />
 </template>
 
 <script>
-  import dataTable from "./other/DataTable.vue";
-  import { getAllSave } from "@/api/save";
-  
-  export default{
-    components: {
-      "data-table": dataTable
+import DataTable from "./other/DataTable.vue";
+import { getAllSave } from "@/api/save";
+
+export default {
+  components: {
+    "data-table": DataTable
+  },
+  data() {
+    return {
+      insertData: [] // 初始化为空数组或适当的默认值
+    };
+  },
+  async created() {
+    await this.fetchDataAndRenderTable();
+  },
+  methods: {
+    async fetchDataAndRenderTable() {
+      this.insertData = await this.getInsertData();
     },
-    methods: {
-      getInsertData(){
+    async getInsertData() {
+      try {
+        const saveData = await getAllSave();
+
         let insertData = [];
-        for (let id of getAllSave()) insertData.push({ classroomID: id });
+        for (let id of saveData) {
+          insertData.push({ classroomID: id });
+        }
+
         return insertData;
+      } catch (error) {
+        console.error('获取插入数据时发生错误:', error);
+        return []; // 或者根据实际情况返回适当的默认值
       }
     }
   }
+};
 </script>
