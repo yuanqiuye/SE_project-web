9c46691b6841ba81250c876b27a31fcdd74359fb
yuanqiuye <chenhsinyuan@gmail.com>
2024-01-05 20:08:46 +0800

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import { getRole } from "@/api/auth";
 
 const routes = [
